ef49330a95a2c96e08f2178c084510defb04073e
Marcel Soltermann <marcel.soltermann@students.fhnw.ch>
2020-05-11 15:12:08 +0200

    [CHG] created helloworld tkinter GUI without action

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,35 @@
 
 import tkinter
 top = tkinter.Tk()
+top.title("Hello stranger")
+
+
+
 # Code to add widgets will go here...
-hello = tkinter.Button ( top, text="Begrüssung" )
-hello.pack()
+
+frame1 = tkinter.Frame(top)
+frame1.pack(padx=10, pady=10)
+
+bgtext = tkinter.StringVar()
+label = tkinter.Label( frame1, textvariable=bgtext )
+bgtext.set("""Guten Tag! \n Dies ist ein Hello World Programm! 
+Für eine persönliche Begrüssung, geben Sie Ihren Namen in das Textfeld ein und klicken anschliessend auf den Knopf.\n""")
+label.pack(padx=10, pady=10)
+
+frame2 = tkinter.Frame(top)
+frame2.pack()
+
+nameLabel = tkinter.Label(frame2, text = "Ihr Name:")
+nameLabel.pack( side = tkinter.LEFT, padx=10, pady=10)
+nameEntry = tkinter.Entry(frame2, bd = 5)
+nameEntry.pack(side = tkinter.RIGHT, padx=10, pady=10)
+
+frame3 = tkinter.Frame(top)
+frame3.pack()
+
+hello = tkinter.Button ( frame3, text="Begrüssung" )
+
+hello.pack(side=tkinter.BOTTOM , padx=10, pady=10)
+
 top.mainloop()
+
